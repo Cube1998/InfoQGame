f3e8605c3088be2b352eea471f46d32e0907f81a
CubeZ <VictorySV29@gmail.com>
2018-05-28 09:32:38 +0800

    None

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include "Game.h"
+
+/*
+Tutorial topics:
+Review/apply a bunch of previously learned concepts
+New Topic:
+QPen, QBrush, setPen(), setBrush()
+*/
+
+Game * game;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    game = new Game();
+    game->show();
 
     return a.exec();
 }
